e4f3a3db1cbb336efc3ef86bf8b74751c7049ff1
victorfort5551219-creator <victor.fort5551219@serra.pe.senac.br>
2026-08-19 08:00:06 -0300

    alteração

--- src/Library.java
+++ src/Library.java
@@ -1,3 +1,29 @@
 public class library{
-    
+    import java.util.ArrayList;
+
+public class Library {
+
+    private ArrayList<Book> books = new ArrayList<>();
+    private ArrayList<User> users = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void listBooks() {
+        for (Book b : books) {
+            System.out.println(b);
+        }
+    }
+
+    public void listUsers() {
+        for (User u : users) {
+            u.listarInformacoes();
+        }
+    }
+}
 }
